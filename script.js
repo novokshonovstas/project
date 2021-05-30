3e7b9e0b256478121d5202a6715c2ec300342778
Stas <wstas@ymail.com>
2021-05-30 21:44:57 +0300

    uncomment in script

--- script.js
+++ script.js
@@ -13,15 +13,15 @@
 
 
 // Пример работы переменных, интерполяции, модальных окон и массивов
-// let nameUser = 'Стас',
-//    age = 26;
-// let project = [];
+let nameUser = 'Стас',
+   age = 26;
+let project = [];
 
-// project[0] = prompt('Как ваше имя?');
-// project[1] = prompt('Как ваша фамилия');
-// project[2] = prompt('Сколько вам лет?');
-// alert(`Привет, меня зовут ${nameUser}, мне ${age} лет! `)
-// document.write(project);
+project[0] = prompt('Как ваше имя?');
+project[1] = prompt('Как ваша фамилия');
+project[2] = prompt('Сколько вам лет?');
+alert(`Привет, меня зовут ${nameUser}, мне ${age} лет! `)
+document.write(project);
 
 
 // alert('Hello');
